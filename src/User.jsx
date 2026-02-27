4bd39184c9513a7e7af33a7edea29fcf029bfba9
Atharv Sharma <sharmaatharv7463@gmail.com>
2026-02-27 10:43:47 +0530

    first commit

---
+++ src/User.jsx
@@ -0,0 +1,13 @@
+function User() {
+  return (
+    <div>
+      <h1>User name is Atharv Sharma</h1>
+      <h1>User name is Atharv Sharma</h1>
+      <h5>User name isAtharv Sharma</h5>
+      <h1>User name is Atharv Sharma</h1>
+      <h3>User name is Atharv Sharma</h3>
+    </div>
+  );
+}
+
+export default User;
