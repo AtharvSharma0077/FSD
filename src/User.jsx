aca69fa72e3c3c78187ff4a128286ba8469af72d
Atharv Sharma <sharmaatharv7463@gmail.com>
2026-02-27 21:56:17 +0530

    added exp 5

--- src/User.jsx
+++ src/User.jsx
@@ -2,7 +2,7 @@ function User() {
   return (
     <div>
       <h1>User name is Atharv Sharma</h1>
-      <h1>User name is Atharv Sharma</h1>
+      <h2>User name is Atharv Sharma</h2>
       <h5>User name isAtharv Sharma</h5>
       <h1>User name is Atharv Sharma</h1>
       <h3>User name is Atharv Sharma</h3>
